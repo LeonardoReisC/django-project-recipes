acecad120ee0a1fbed0407cf2794fb4b590e02ac
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2023-10-04 01:51:07 -0300

    creates a header using partials

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -7,7 +6,7 @@ from django.http import HttpResponse
 def home(request):
     return render(
         request,
-        'recipes/home.html',
+        'recipes/pages/home.html',
         context={
             'name': 'Leonardo',
         }

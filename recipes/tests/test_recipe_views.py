8d92d404cc25ed08a10049a66237e53e58e64720
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2023-10-27 21:34:26 -0300

    creates fixtures for testing

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipeViewsTest(TestCase):
@@ -31,6 +32,31 @@ class RecipeViewsTest(TestCase):
             'No recipes found',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='Fname',
+            last_name='Lname',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            title='Recipe Tile',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutes',
+            servings=5,
+            servings_unit='Servings',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+            category=category,
+            author=author,
+        )
+        ...
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(

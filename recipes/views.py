b2677e0d1de36a6c0699fdfda8d354ed273e549d
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2023-12-30 23:32:14 -0300

    add search_term to title

--- recipes/views.py
+++ recipes/views.py
@@ -55,7 +55,7 @@ def recipe(request, id):
 
 
 def search(request):
-    search_term = request.GET.get('q')
+    search_term = request.GET.get('q', '').strip()
 
     if not search_term:
         raise Http404()
@@ -63,4 +63,8 @@ def search(request):
     return render(
         request,
         'recipes/pages/search.html',
+        context={
+            'title': f'Search for "{search_term}" | ',
+            'search_term': search_term,
+        }
     )

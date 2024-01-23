97d93ae0020604efbc199b3a327a2595a37fa6c3
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-01-23 20:06:21 -0300

    test pagination first ranges

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from parameterized import parameterized
 from utils.pagination import make_pagination_range
 
 
@@ -10,4 +11,13 @@ class PaginationTest(TestCase):
             current_page=1
         )
 
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+    @parameterized.expand([1, 2])
+    def test_make_pagination_range_is_static_with_first_pages(self, page):  # noqa: E501
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            page_qty=4,
+            current_page=page
+        )
         self.assertEqual([1, 2, 3, 4], pagination)

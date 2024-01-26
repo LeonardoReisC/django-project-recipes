41252bccdf1be63de0efd3843d5ac078bc81b3d1
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-01-26 18:24:29 -0300

    add paginator to template

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,8 @@ from django.http.response import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.db.models import Q
 from django.core.paginator import Paginator
+
+from utils.pagination import make_pagination_range
 from .models import Recipe
 
 # Create your views here.
@@ -12,15 +14,25 @@ def home(request):
         .filter(is_published=True) \
         .order_by('-id')
 
-    current_page = request.GET.get('page', 1)
-    paginator = Paginator(recipes, 6)
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+    paginator = Paginator(recipes, 3)
     page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page,
+    )
 
     return render(
         request,
         'recipes/pages/home.html',
         context={
             'recipes': page_obj,
+            'pagination_range': pagination_range
         }
     )
 

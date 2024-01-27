966ce095ee4a443b922c7f9976d5b09bb7411810
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-01-27 20:54:21 -0300

    set form fields' labels, widgets ...

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,26 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        labels = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'This e-mail must be valid.'
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty.'
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }

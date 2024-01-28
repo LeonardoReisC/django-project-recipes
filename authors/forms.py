4462638785d16d9de22d197202c0526de272bc97
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-01-28 19:06:00 -0300

    overwrite form fields

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,44 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_value):
+    field.widget.attrs[attr_name] = attr_new_value
+
+
+def add_placeholder(field, placeholder_value):
+    add_attr(field, 'placeholder', placeholder_value)
+
+
 class RegisterForm(forms.ModelForm):
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Type your password here'
+        }),
+        error_messages={
+            'required': 'This field must not be empty.'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Type your password here again'
+        })
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['first_name'], 'e.g. Leonardo')
+        add_placeholder(self.fields['last_name'], 'e.g. Reis')
+        add_placeholder(self.fields['username'], 'Type your username here')
+        add_placeholder(self.fields['email'], 'Type your email here')
+
     class Meta:
         model = User
         fields = [

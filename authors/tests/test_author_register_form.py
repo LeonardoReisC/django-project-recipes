b3ab01dafeb4c6970efc71f25b5cd6c86842b6c4
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-01-28 21:46:14 -0300

    test form fields' help_text

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -19,3 +19,17 @@ class AuthorRegisterFormUnitTest(TestCase):
         current_placeholder = form[field].field.widget.attrs['placeholder']
 
         self.assertEqual(placeholder, current_placeholder)
+
+    @parameterized.expand([
+        ('email', 'This e-mail must be valid.'),
+        ('password', (
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )),
+    ])
+    def test_fields_help_text_is_correct(self, field, help_text):
+        form = RegisterForm()
+        current_help_text = form[field].field.help_text
+
+        self.assertEqual(help_text, current_help_text)

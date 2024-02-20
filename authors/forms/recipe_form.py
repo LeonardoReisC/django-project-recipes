36ac2cdbfce0785f1e3207fb470d260955e5f75c
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-02-20 20:35:06 -0300

    validate recipe form

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,12 +1,17 @@
+from collections import defaultdict
+from django.core.exceptions import ValidationError
 from django import forms
 
 from recipes.models import Recipe
 from utils.django_forms import add_attr
+from utils.strings import is_positive_number
 
 
 class RecipeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self._my_errors = defaultdict(list)
 
         add_attr(self.fields['title'], 'class', 'span-2')
         add_attr(self.fields['description'], 'class', 'span-2')
@@ -46,3 +51,45 @@ class RecipeForm(forms.ModelForm):
                 }
             ),
         }
+
+    def clean_title(self):
+        data = self.cleaned_data["title"]
+
+        if len(data) < 5:
+            self._my_errors['title'].append('Must have at least 5 characters.')
+
+        return data
+
+    def clean_preparation_time(self):
+        data = self.cleaned_data["preparation_time"]
+
+        if not is_positive_number(data):
+            self._my_errors['preparation_time'].append(
+                'Must be a positive number.'
+            )
+
+        return data
+
+    def clean_servings(self):
+        data = self.cleaned_data["servings"]
+
+        if not is_positive_number(data):
+            self._my_errors['servings'].append(
+                'Must be a positive number.'
+            )
+
+        return data
+
+    def clean(self, *args, **kwargs):
+        cleaned_data = super().clean(*args, **kwargs)
+
+        title = cleaned_data.get('title')
+        description = cleaned_data.get('description')
+
+        if title == description:
+            self._my_errors['description'].append('Cannot be equal to title.')
+
+        if self._my_errors:
+            raise ValidationError(self._my_errors)
+
+        return cleaned_data

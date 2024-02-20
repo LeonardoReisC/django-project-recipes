0bd3247cd34fcc30353a80872eef51672dfdee85
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-02-20 19:48:15 -0300

    add recipe creation form

--- authors/views.py
+++ authors/views.py
@@ -137,6 +137,7 @@ def dashboard_recipe_edit(request, id):
         recipe.preparation_steps_is_html = False
         recipe.is_published = False
         recipe.save()
+
         messages.success(request, 'Recipe saved successfully.')
         return redirect('authors:dashboard')
 
@@ -145,5 +146,33 @@ def dashboard_recipe_edit(request, id):
         'authors/pages/dashboard_recipe.html',
         context={
             'form': form,
+        }
+    )
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_create(request):
+    form = RecipeForm(
+        request.POST or None,
+        files=request.FILES or None,
+    )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+        recipe.save()
+
+        messages.success(request, 'Recipe created successfully.')
+        return redirect('authors:dashboard')
+
+    return render(
+        request,
+        'authors/pages/dashboard_recipe.html',
+        context={
+            'form': form,
+            'form_action': reverse('authors:dashboard_recipe_create')
         }
     )

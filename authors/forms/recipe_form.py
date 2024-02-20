bbc7f722b317277fb0775ee7de6a5ad151d5dcd7
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-02-20 16:49:39 -0300

    fix recipe form css grid

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,9 +1,17 @@
 from django import forms
 
 from recipes.models import Recipe
+from utils.django_forms import add_attr
 
 
 class RecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        add_attr(self.fields['title'], 'class', 'span-2')
+        add_attr(self.fields['description'], 'class', 'span-2')
+        add_attr(self.fields['preparation_steps'], 'class', 'span-2')
+        add_attr(self.fields['cover'], 'class', 'span-2')
 
     class Meta:
         model = Recipe

1b0b5b4a1a07f2c541ac118e9a43f7e7838e8d70
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-02-23 15:08:08 -0300

    theory: add generic foreign key to tag model

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,33 @@
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
+from django.utils.text import slugify
+from random import SystemRandom
+import string
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.CharField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            rand_letters = ''.join(
+                SystemRandom().choices(
+                    string.ascii_letters + string.digits,
+                    k=5
+                )
+            )
+
+            self.slug = slugify(f'{self.name}-{rand_letters}')
+
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name

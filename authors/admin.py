cec097592dfc6e8b163b36cd1c522a6638be36bd
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-02-24 18:43:40 -0300

    create Profile model

--- authors/admin.py
+++ authors/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Profile
+
 # Register your models here.
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = 'author_first_name',
+
+    @admin.display(ordering='author__first_name')
+    def author_first_name(self, obj):
+        return obj.author.first_name

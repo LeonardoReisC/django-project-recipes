4dbf4f94f6fd59d987c3f479eb30399e81884bf2
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-02-29 22:16:24 -0300

    fix serializers path

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from recipes.models import Recipe
-from recipes.views.serializers import RecipeSerializer
+from recipes.serializers import RecipeSerializer
 
 
 @api_view()

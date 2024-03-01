360e4fdc14aadf6937457afc1949dc39af44c14e
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-03-01 18:45:42 -0300

    add RetrieveUpdateDestroyAPIView inheritance

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,10 +1,9 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from recipes.models import Recipe
 from recipes.serializer import RecipeSerializer
@@ -22,40 +21,10 @@ class RecipeAPIV2List(ListCreateAPIView):
     pagination_class = RecipeAPUV2Pagination
 
 
-class RecipeAPIV2Detail(APIView):
-    def get_recipe(self, pk):
-        return get_object_or_404(Recipe.objects.get_published(), pk=pk)
-
-    def get(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            context={
-                'request': request
-            }
-        )
-        return Response(serializer.data)
-
-    def patch(self, request, pk):
-        recipe = self.get_recipe(pk)
-        recipe = get_object_or_404(Recipe.objects.get_published(), pk=pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            data=request.data,
-            context={
-                'request': request
-            },
-            partial=True,
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, pk):
-        recipe = self.get_recipe(pk)
-        recipe = get_object_or_404(Recipe.objects.get_published(), pk=pk)
-        recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class RecipeAPIV2Detail(RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPUV2Pagination
 
 
 @api_view()

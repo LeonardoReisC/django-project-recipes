ebc705d4fc2f1500d6a2372047506626aac20bf9
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-02-29 23:58:26 -0300

    change serializers to ModelSerializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,23 +1,24 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from tag.models import Tag
+from recipes.models import Recipe
 
 
-class RecipeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published')
-    preparation = serializers.SerializerMethodField()
-    category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(),
-    )
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'description',
+            'preparation', 'author', 'category',
+            'tags', 'public'
+        ]
+
+    public = serializers.BooleanField(source='is_published', read_only=True)
+    preparation = serializers.SerializerMethodField(read_only=True)
+    category = serializers.StringRelatedField(read_only=True)
     tags = serializers.HyperlinkedRelatedField(
-        queryset=Tag.objects.all(),
         view_name='recipes:recipes_api_v2_tag',
         many=True,
+        read_only=True,
     )
 
     def get_preparation(self, recipe):

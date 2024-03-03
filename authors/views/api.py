788f6a605a1a97756144952f7d3e196e802cadde
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-03-02 21:42:09 -0300

    create /me custom router

--- authors/views/api.py
+++ authors/views/api.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from authors.serializer import AuthorSerializer
@@ -14,3 +16,11 @@ class AuthorAPIV2ViewSet(ReadOnlyModelViewSet):
     def get_queryset(self):
         qs = User.objects.filter(username=self.request.user.username)
         return qs
+
+    @action(methods=['get'], detail=False)
+    def me(self, request, *args, **kwargs):
+        obj = self.get_queryset().first()
+        serializer = self.get_serializer(
+            instance=obj,
+        )
+        return Response(serializer.data)
